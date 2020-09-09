d870d67301ce26cb580d8d51c47adda94ab9f737
Hai Hoang <49620200+hoanghai961997@users.noreply.github.com>
2020-09-10 00:49:26 +0300

    Delete weaponfactorytweakdata.lua

--- Quadstacked Mag 308/Hooks/weaponfactorytweakdata.lua
+++
@@ -1,8 +0,0 @@
-Hooks:PostHook( WeaponFactoryTweakData, "init", "contractor308", function(self)
-
-self.parts.wpn_fps_snp_tti_m_quad.override_weapon_multiply = {
-	CLIP_AMMO_MAX = 2, 
-	AMMO_MAX = 1.5	
-	}
-		
-end )
