f3b6565937a98e971dd5637b3662a1943ac26f4f
Hai Hoang <49620200+hoanghai961997@users.noreply.github.com>
2020-09-09 07:20:15 +0300

    Add files via upload

---
+++ jocelineattachment/Hooks/weapontweakdata.lua
@@ -0,0 +1,22 @@
+Hooks:PostHook(WeaponTweakData, "init", "jocelineattachment", function(self)
+	self:SetupAttachmentPoint( "b682", {
+        name = "a_o",
+        base_a_obj = "a_b",
+        part_attach_data = {
+	{"wpn_fps_shot_b682_b_short",
+	"wpn_fps_shot_b682_b_long"},
+	"g_barrel_lod0"},
+	position = Vector3( 0, 20, 5.7 ),
+	rotation = RotationCAP(0, 0, 0)
+   	})
+	self:SetupAttachmentPoint( "b682", {
+        name = "a_fl",
+        base_a_obj = "a_b",
+        part_attach_data = {
+	{"wpn_fps_shot_b682_b_short",
+	"wpn_fps_shot_b682_b_long"},
+	"g_barrel_lod0"},
+	position = Vector3( 1.9, 17, 1 ),
+	rotation = RotationCAP(0, 0, 0)
+   	})
+end)
