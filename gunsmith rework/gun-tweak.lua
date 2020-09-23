8660bf22acd80f46b8529dc0b02d6b8989d74973
Hai Hoang <49620200+hoanghai961997@users.noreply.github.com>
2020-09-23 17:49:22 +0300

    added joceline

--- gunsmith rework/gun-tweak.lua
+++ gunsmith rework/gun-tweak.lua
@@ -78,7 +78,27 @@ self.benelli.AMMO_MAX = 63 -- total ammo.
 --Raven--
 --Steakout 12G--
 --Predator 12G--
---Joceline O/U 12G-- 
+--Joceline O/U 12G--
+self:SetupAttachmentPoint( "b682", {
+	name = "a_o",
+	base_a_obj = "a_b",
+	part_attach_data = {
+		{"wpn_fps_shot_b682_b_short",
+		"wpn_fps_shot_b682_b_long"},
+		"g_barrel_lod0"},
+	position = Vector3( 0, 20, 5.7 )})
+self:SetupAttachmentPoint( "b682", {
+	name = "a_fl",
+	base_a_obj = "a_b",
+	part_attach_data = {
+		{"wpn_fps_shot_b682_b_short",
+		"wpn_fps_shot_b682_b_long"},
+		"g_barrel_lod0"},
+	position = Vector3( 1.9, 17, 1 )})
+self:SetupAttachmentPoint( "b682", {
+	name = "a_body_custom",
+	base_a_obj = "a_body",
+	position = Vector3( -0.2, 10, -4.8 ), rotation = RotationCAP(0, 0, -180)})
 ----------Sniper Rilfe---------
 --Platypus 70--
 --Rattlesnake--
--- gunsmith rework/mod-tweak.lua
+++ gunsmith rework/mod-tweak.lua
@@ -281,8 +281,79 @@ self.wpn_fps_ass_amcar.override = {
 	wpn_fps_m4_upg_m_quick = {stats = {extra_ammo = 5, reload = 5, concealment = -2}},
 	wpn_fps_upg_m4_m_quad = {stats = {extra_ammo = 20, recoil = 1, reload = -5, concealment = -4}}
 }
-
-
+--Joceline O/U 12G--
+self.wpn_fps_shot_b682.override= {
+	wpn_fps_upg_o_t1micro = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_docter = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_cmore = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_cs = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_reflex = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_rx01 = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_eotech = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_eotech_xps = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_rx30 = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_aimpoint = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_aimpoint_2 = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_specter = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_acog = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_spot = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_uh = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_bmg = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_fc1 = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_sig = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_xpsg33_magnifier = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(0, -23, -4.4),rotation = Rotation(0, 0, 0)}}},
+	wpn_fps_upg_o_45rds = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(-3.55, -23, -13.65),rotation = Rotation(0, 0, -45)}}},
+	wpn_fps_upg_o_45rds_v2 = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(-3.55, -23, -13.65),rotation = Rotation(0, 0, -45)}}},
+	wpn_fps_upg_o_45steel = {stance_mod = {wpn_fps_shot_b682 = {translation = Vector3(-3.55, -23, -13.65),rotation = Rotation(0, 0, -45)}}},
+	wpn_fps_shot_r870_body_rack = {a_obj ="a_body_custom"}
+}
+self.wpn_fps_shot_b682.adds = {
+	wpn_fps_upg_o_t1micro = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_docter = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_cmore = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_cs = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_reflex = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_rx01 = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_eotech = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_eotech_xps = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_rx30 = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_aimpoint_2 = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_aimpoint = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_specter = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_acog = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_spot = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_uh = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_bmg = {"wpn_fps_shot_r870_ris_special"},
+	wpn_fps_upg_o_fc1 = {"wpn_fps_shot_r870_ris_special"}
+}
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_t1micro")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_docter")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_cmore")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_cs")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_reflex")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_rx01")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_eotech")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_eotech_xps")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_rx30")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_aimpoint")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_aimpoint_2")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_specter")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_acog")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_spot")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_sig")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_xpsg33_magnifier")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_45rds_v2")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_45rds")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_fl_ass_smg_sho_surefire")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_fl_ass_smg_sho_peqbox")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_fl_ass_laser")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_fl_ass_peq15")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_fl_ass_utg")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_45steel")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_shot_r870_body_rack")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_bmg")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_fc1")
+table.insert(self.wpn_fps_shot_b682.uses_parts, "wpn_fps_upg_o_uh")
 
 self.wpn_fps_ass_m16.override.wpn_fps_upg_m4_m_pmag = {stats = {extra_ammo = 5, concealment = -1}} -- AMR16 Tactical Mag
 self.wpn_fps_ass_m16.override.wpn_fps_upg_m4_m_l5 = {stats = {extra_ammo = 5, concealment = -1}} -- AMR16 L5 Mag
